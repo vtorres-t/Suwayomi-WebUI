5ea5dca560335627efa9c3e200fb842e3ea57338
Vicente <vicente@per>
2026-06-19 10:24:14 +0200

    workflow_dispatch

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -8,11 +8,13 @@
 
 export default {
     '*.{ts,tsx,js,jsx}': [
-        'oxfmt --write',
-        // Encapsula el comando en una función para asegurar que el operador lógico "|| true" quede al final de la ejecución
-        (filenames) => `oxlint --fix ${filenames.join(' ')} || true`,
-        () => 'pnpm tsc',
+        // Si hay archivos, corre oxfmt. Si la lista está vacía, devuelve un array vacío (no hace nada)
+        (filenames) => filenames.length > 0 ? `oxfmt --write ${filenames.join(' ')}` : [],
+        // Si hay archivos, corre oxlint.
+        (filenames) => filenames.length > 0 ? `oxlint --fix ${filenames.join(' ')}` : [],
+        () => 'pnpm tsc'
     ],
-    '*.{json,md,yml,yaml,css,scss,html,graphql}': 'oxfmt --write',
+    '*.{json,md,yml,yaml,css,scss,html,graphql}': (filenames) =>
+        filenames.length > 0 ? `oxfmt --write ${filenames.join(' ')}` : [],
     '*.json': () => 'pnpm tsc',
 };
