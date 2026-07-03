199921c67b6ac8a38a2ee0f7fc264750e805713a
Vicente <vicente@per>
2026-07-03 11:26:03 +0200

    Fix SYNC_SUBSCRIPTION

--- src/lib/graphql/sync/SyncSubscription.ts
+++ src/lib/graphql/sync/SyncSubscription.ts
@@ -7,12 +7,12 @@
  */
 
 import gql from 'graphql-tag';
-import {SYNC_STATUS_FIELDS} from '@/lib/graphql/sync/SyncFragments.ts';
+import { SYNC_STATUS_FIELDS } from '@/lib/graphql/sync/SyncFragments.ts';
 
 export const SYNC_SUBSCRIPTION = gql`
     ${SYNC_STATUS_FIELDS}
 
-    subscription SYNC_SUBSCRIPTION {
+    subscription SYNC {
         syncStatusChanged {
             ...SYNC_STATUS_FIELDS
         }
