fa08328edfac00931afac3a7983529df4bebcbe9
Vicente <vicente@per>
2026-08-03 13:29:15 +0200

    Remove DebugInfo

--- src/features/settings/screens/About.tsx
+++ src/features/settings/screens/About.tsx
@@ -22,8 +22,6 @@ import { VersionInfo } from '@/features/app-updates/components/VersionInfo.tsx';
 import { getErrorMessage } from '@/lib/HelperFunctions.ts';
 import { epochToDate } from '@/base/utils/DateHelper.ts';
 import { useAppTitle } from '@/features/navigation-bar/hooks/useAppTitle.ts';
-import { DebugInformation } from '@/features/settings/components/DebugInformation.tsx';
-import Stack from '@mui/material/Stack';
 
 export function About() {
     const { t } = useLingui();
@@ -155,17 +153,6 @@ export function About() {
                         secondary={`(${aboutWebUI.repoType.toUpperCase()}): ${aboutWebUI.repoUrl}`}
                     />
                 </ListItemLink>
-            </List>
-            <List
-                subheader={
-                    <ListSubheader component="div" id="about-webui-info">
-                        {t`Debug information`}
-                    </ListSubheader>
-                }
-            >
-                <Stack sx={{ px: 2, py: 1 }}>
-                    <DebugInformation />
-                </Stack>
             </List>
         </List>
     );
